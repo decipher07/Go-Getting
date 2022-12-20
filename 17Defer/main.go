2a661b0441a61bfdccd1e0c8955a207ca4779b79
decipher07 <social.deej@gmail.com>
2022-12-21 02:12:58 +0530

    modified:   17Defer/main.go

--- 17Defer/main.go
+++ 17Defer/main.go
@@ -3,5 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	// fmt.Println("Hello World")
+	defer fmt.Println("World")
+	defer fmt.Println("One")
+	defer fmt.Println("Two")
+	fmt.Println("Hello")
+	myDefer()
+}
+
+func myDefer() {
+	for i := 0; i < 5; i++ {
+		defer fmt.Println(i)
+	}
 }
