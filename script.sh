c5f2043fd7bd4ac8c1a6b5095e85a3e50b7f0e74
decipher07 <social.deej@gmail.com>
2022-12-20 21:53:49 +0530

    update script

--- script.sh
+++ script.sh
@@ -1,3 +1,7 @@
 result=$(git status | grep ".*\.go$" )
 
 git add . && git commit -m "$result" > /dev/null 2>&1
+
+result=$(git status | grep ".*\.go$" )
+
+git add . && git commit -m "$result" > /dev/null 2>&1
