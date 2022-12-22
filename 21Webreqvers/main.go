8b6df0627be6adfe41b345a6919ef97c18de472f
decipher07 <social.deej@gmail.com>
2022-12-22 16:58:33 +0530

    modified:   21Webreqvers/main.go

--- 21Webreqvers/main.go
+++ 21Webreqvers/main.go
@@ -7,14 +7,16 @@ import (
 	"strings"
 )
 
+const myurl = "http://3.109.139.40:3000/"
+
 func main() {
 	fmt.Println("Hello World")
-	PerformGetRequest()
+	// PerformGetRequest()
+	performPostRequest()
 }
 
 func PerformGetRequest() {
-	const myurl = "http://3.109.139.40:3000/ping"
-
+	myurl := myurl + "ping"
 	response, err := http.Get(myurl)
 
 	if err != nil {
@@ -32,4 +34,27 @@ func PerformGetRequest() {
 	fmt.Println("ByteCount is: ", byteContent)
 	fmt.Println(responseString.String())
 
+	defer response.Body.Close()
+}
+
+func performPostRequest() {
+	pathToPost := myurl + "payment/makepayment"
+
+	requestBody := strings.NewReader(`
+		{
+			"userId": "98e3c010-1573-4d44-a23a-323a0dd25f56",
+			"month": 11
+		}
+	`)
+
+	response, err := http.Post(pathToPost, "application/json", requestBody)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer response.Body.Close()
+	content, _ := ioutil.ReadAll(response.Body)
+
+	fmt.Println(string(content))
 }
