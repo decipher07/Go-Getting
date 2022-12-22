40bd5e4f9dabac07e80dc0fe9125a3065195db1f
decipher07 <social.deej@gmail.com>
2022-12-22 23:43:29 +0530

    new file:   23mymodules/main.go 	new file:   23mymodules/vendor/github.com/gorilla/mux/doc.go 	new file:   23mymodules/vendor/github.com/gorilla/mux/middleware.go 	new file:   23mymodules/vendor/github.com/gorilla/mux/mux.go 	new file:   23mymodules/vendor/github.com/gorilla/mux/regexp.go 	new file:   23mymodules/vendor/github.com/gorilla/mux/route.go 	new file:   23mymodules/vendor/github.com/gorilla/mux/test_helpers.go

---
+++ 23mymodules/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	fmt.Println("Hello World")
+	greeter()
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", serveHome).Methods("GET")
+
+	log.Fatal(http.ListenAndServe(":4000", r))
+}
+
+func greeter() {
+	fmt.Println("Hello from Greeter")
+}
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>Hello World From DJ</h1>"))
+}
