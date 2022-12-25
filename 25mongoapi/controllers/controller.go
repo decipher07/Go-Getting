dd61e281e63fa063b83019fa7a958dc468f57478
decipher07 <social.deej@gmail.com>
2022-12-25 16:58:38 +0530

    modified:   25mongoapi/controllers/controller.go

--- 25mongoapi/controllers/controller.go
+++ 25mongoapi/controllers/controller.go
@@ -6,6 +6,8 @@ import (
 	"log"
 
 	model "github.com/decipher07/mongoapi/models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -52,4 +54,26 @@ func insertOneMovie(movie model.Netflix) {
 	}
 
 	fmt.Println("Inserted 1 movie in Database with id : ", inserted.InsertedID)
+}
+
+/* Update One Record */
+func updateOneMovie(movieId string) {
+
+	/* Get the Id for the updates */
+	id, _ := primitive.ObjectIDFromHex(movieId)
+
+	/* Filter Query */
+	filter := bson.M{"_id": id}
+
+	/* Update Query */
+	update := bson.M{"$set": bson.M{"watched": true}}
+
+	/* Result and Error for the Collections */
+	result, err := collection.UpdateOne(context.Background(), filter, update)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Modified Count: ", result.MatchedCount)
 }
