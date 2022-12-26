2020a83d5b1c88109f8160d597948868fa836693
decipher07 <social.deej@gmail.com>
2022-12-27 02:41:19 +0530

    modified:   26goroutines/main.go

--- 26goroutines/main.go
+++ 26goroutines/main.go
@@ -2,12 +2,30 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"sync"
 	"time"
 )
 
+var wg sync.WaitGroup
+
 func main() {
-	go greeter("Hello")
-	greeter("World")
+	/* 	go greeter("Hello")
+	   	greeter("World") */
+
+	websiteList := []string{
+		"http://lco.dev",
+		"http://google.com",
+		"http://github.com",
+		"http://fb.com",
+	}
+
+	for _, web := range websiteList {
+		wg.Add(1)
+		go getStatusCode(web)
+	}
+
+	wg.Wait()
 }
 
 func greeter(s string) {
@@ -15,4 +33,10 @@ func greeter(s string) {
 		time.Sleep(3 * time.Millisecond)
 		fmt.Println(s)
 	}
+}
+
+func getStatusCode(endpoint string) {
+	res, _ := http.Get(endpoint)
+	fmt.Printf("%d Status Code for %s\n", res.StatusCode, endpoint)
+	defer wg.Done()
 }
